a21f07653c712422560a52297677593b737eb586
maali2403 <momaali1717@gmail.com>
2024-09-16 17:25:20 +0200

    Add files via upload

---
+++ Øvelse 5 fremlæggelse.py
@@ -0,0 +1,12 @@
+
+x = 21.2+20.5+21.6+22.1+21.5
+#laver en x variabel og tildeler den alle værdierne kl 18
+
+y = x/6
+# laver en y variabel, som er summen/gennemsnittet af alle værdierne kl. 18
+
+svar = f"gennemsnitstemperaturen kl 18:00 er {y:.1f}"
+# laver en f string som bruges til at printe en forklaring.
+#{y:.1f}, indregner 1 decimal. y er funktionen, og .1f indregner 1 decimal, hvor at f er float
+
+print(svar)
