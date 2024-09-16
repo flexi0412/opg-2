a82f87d4a118805938baba45170669bd3f42aa71
Gebhep <161262262+Gebhep@users.noreply.github.com>
2024-09-16 17:04:07 +0200

    Add files via upload

---
+++ Øvelse-1.1.py
@@ -0,0 +1,17 @@
+# Øvelser 1.
+# 1. Lav typecasting fra floatingpoints til integer(heltal)
+
+# Float datasæt
+float1 =  5799.7345
+float2 = 1111.1111
+float3 = 339.993
+float4 = 1.05
+
+# Laver variabler der omdanner data float til int
+float_res1 = int(float1)
+float_res2 = int(float2)
+float_res3 = int(float3)
+float_res4 = int(float4)
+
+# Printer datatype og vores nye integer variabler
+print(type(float_res1), float_res1, type(float_res2), float_res2, type(float_res3), float_res3, type(float_res4), float_res4)
